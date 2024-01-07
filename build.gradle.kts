5c36ce8485eab26a575c72746af9a96350463371
Daniel THIRION <defvs.daniel@gmail.com>
2024-01-07 16:23:32 +0100

    Init project

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "1.9.21"
+}
+
+group = "dev.defvs"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(8)
+}
